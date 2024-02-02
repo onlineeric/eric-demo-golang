3f0ec9d694432b01c10318c7fff8408de66bd5e4
Eric Cheng <online.eric@gmail.com>
2024-02-02 23:14:04 +1300

    minor code improvement

--- main.go
+++ main.go
@@ -12,11 +12,15 @@ var AppVersion = "0.1.0"
 func main() {
 	router := gin.Default()
 
+	setupRouters(router)
+
+	router.Run("0.0.0.0:8080")
+}
+
+func setupRouters(router *gin.Engine) {
 	router.GET("/status", getStatus)
 	routers.SetSimpleRouters(router)
 	routers.SetBenchmarkRouters(router)
-
-	router.Run("0.0.0.0:8080")
 }
 
 func getStatus(c *gin.Context) {
